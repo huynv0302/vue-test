0457323562f0faae661e2aafbc2c6aa55105e50b
huynv0302 <huynv0302@gmail.com>
2019-04-16 11:43:01 +0700

    edit

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 Route::get('/', 'client\HomeController@home');
 
-Route::get('/Home', 'client\HomeController@home');
+Route::get('/home', 'client\HomeController@home');
 
 Route::get('login', 'client\AccountController@login');
 Route::post('loginpost', 'client\AccountController@postLogin');
 Route::get('logout', 'client\AccountController@logout');
 
-// mobile framework7
+Route::get('json', function(){
+    return response()->json(['data' => 'hello asdas as']);
+});
+
 Route::get('test',function(){
     return view('client.mobile.home');
 });
