e0b37594440591e342a0acbdde5855718816e46a
huynv0302 <huynv0302@gmail.com>
2019-04-16 11:13:43 +0700

    edit

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1799,7 +1799,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
   mounted: function mounted() {
-    console.log('Component mounted adscsad asd.');
+    console.log('Component mounted adscsad asd. asd');
   },
   data: function data() {
     return {
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -35,7 +35,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted adscsad asd.')
+            console.log('Component mounted adscsad asd. asd')
         },
         data() {
             return {
